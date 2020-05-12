bc05230cabfa5242f689e98536ba6a5a2355beed
Farid C. Baxishli <Farid.Baxishli@kapitalbank.az>
2020-05-12 17:38:36 +0400

    twitter-app

---
+++ constants.py
@@ -0,0 +1,6 @@
+CONSUMER_KEY = 'KCNCGmKZ8zGjqaXgJ1081RhHd'
+CONSUMER_SECRET = 'BwtxH8zFReS6jlwmiuFB8u5Csdbk8RVtsUfU7bIylbF7oqOqFZ'
+
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
